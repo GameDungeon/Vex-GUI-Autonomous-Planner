6ed59c5044d6a97c2ce24e89ec5b8fdcbfac7a63
GameDungeon <60719255+GameDungeon@users.noreply.github.com>
2022-11-08 19:31:40 -0600

    GUI Changes

--- src/ts/robot.ts
+++ src/ts/robot.ts
@@ -2,13 +2,9 @@ import * as Units from "./units";
 
 let width_input = <HTMLInputElement>document.getElementById("width-input")!;
 let height_input = <HTMLInputElement>document.getElementById("height-input")!;
-let speed_input = <HTMLInputElement>document.getElementById("speed-input")!;
-let rot_speed_input = <HTMLInputElement>document.getElementById("rot-speed-input")!;
 
 let width: number = 0;
 let height: number = 0;
-let speed: number = 0;
-let rot_speed: number = 0;
 
 const cord_val = new RegExp('^\\d+.?\\d*\\s?(' + 
     Object.keys(Units.abv_map).concat(Object.keys(Units.unit_map)).join('|') + ')?$'); 
@@ -41,35 +37,5 @@ height_input.addEventListener("blur", () => {
     }
     else {
         height_input.classList.add('incorrect');
-    }
-});
-
-speed_input.addEventListener("blur", () => {
-    let value = speed_input.value;
-    if (cord_val.test(value))
-    {
-        speed_input.classList.remove('incorrect');
-        let out = value.match(cord_split)!;
-        let pos = parseFloat(out[1]);
-        let in_unit = Units.getUnitByName(out[2]);
-        speed = Units.convertToUnit(in_unit, Units.pixels, pos);
-    }
-    else {
-        speed_input.classList.add('incorrect');
-    }
-});
-
-rot_speed_input.addEventListener("blur", () => {
-    let value = rot_speed_input.value;
-    if (cord_val.test(value))
-    {
-        rot_speed_input.classList.remove('incorrect');
-        let out = value.match(cord_split)!;
-        let pos = parseFloat(out[1]);
-        let in_unit = Units.getUnitByName(out[2]);
-        rot_speed = Units.convertToUnit(in_unit, Units.pixels, pos);
-    }
-    else {
-        rot_speed_input.classList.add('incorrect');
     }
 });
