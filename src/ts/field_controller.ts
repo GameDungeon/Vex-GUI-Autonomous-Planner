1c21b0cbe5124ea48c375fcd48345ed8f60cec26
GameDungeon <60719255+GameDungeon@users.noreply.github.com>
2022-11-09 20:12:11 -0600

    Fix Buttons - Insert Prototype

--- src/ts/field_controller.ts
+++ src/ts/field_controller.ts
@@ -4,19 +4,35 @@ import * as Active from "./field/active_layer";
 import * as SideBar from "./side_bar";
 import { selected_point, linePoint } from "./inspector";
 import { Tools } from "./side_bar";
+import { midpoint } from "./utils";
 
-export let points : linePoint[] = [];
+export let points: linePoint[] = [];
+export let midpoints: Konva.Circle[] = []
+
+function deactivateCurrentMode() {
+    switch (SideBar.current_tool) {
+        case Tools.Draw:
+            Active.line.points(Active.line.points().slice(0, -2));
+            break;
+        case Tools.Insert:
+            midpoints.forEach(point => {
+                point.destroy()
+            });
+            break;
+        case Tools.Edit:
+            break;
+    }
+}
 
 function activateEditMode() {
-    if(SideBar.current_tool === Tools.Draw)
-    {
-        Active.line.points(Active.line.points().slice(0, -2));
-    }
+    deactivateCurrentMode();
 
     SideBar.selectTool(document.getElementById("edit"), Tools.Edit);
 }
 
 function activateDrawMode() {
+    deactivateCurrentMode();
+
     var pointerPos = Field.stage.getRelativePointerPosition();
     var x = pointerPos.x;
     var y = pointerPos.y;
@@ -25,9 +41,53 @@ function activateDrawMode() {
 }
 
 function activateInsertMode() {
-    if(SideBar.current_tool === Tools.Draw)
-    {
-        Active.line.points(Active.line.points().slice(0, -2));
+    deactivateCurrentMode();
+
+    for (let i = 0; i < points.length - 1; i++) {
+        let new_point = midpoint(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+
+        var circle = new Konva.Circle({
+            x: new_point[0],
+            y: new_point[1],
+            radius: 10,
+            fill: 'green',
+            draggable: false,
+            strokeWidth: 3
+        });
+
+        circle.on("pointerdown", () => {
+            let new_points = Active.line.points().splice(i*2+2, 0, new_point[0], new_point[1])
+            Active.line.points(new_points);
+
+            var new_real_point = new Konva.Circle({
+                x: new_point[0],
+                y: new_point[1],
+                radius: 10,
+                fill: 'black',
+                draggable: true,
+                strokeWidth: 3
+            });
+
+            var point = new linePoint(new_real_point, points.length);
+
+            new_real_point.on('dragmove', () => {
+                point.drag();
+            });
+
+            new_real_point.on("pointerdown", () => {
+                point.select();
+            })
+
+            points.splice(i*2, 0, point);
+            point.select();
+
+            Active.pointslayer.add(new_real_point);
+
+            activateEditMode();
+        })
+
+        midpoints.push(circle);
+        Active.pointslayer.add(circle);
     }
 
     SideBar.selectTool(document.getElementById("insert"), Tools.Insert);
@@ -39,51 +99,50 @@ function updatePoints() {
     for (var i = 0; i < arrayLength; i++) {
         let current_point = points[i];
         current_point.index = i;
-        linepoints[i*2] = current_point.x;
-        linepoints[i*2+1] = current_point.y;
+        linepoints[i * 2] = current_point.x;
+        linepoints[i * 2 + 1] = current_point.y;
     }
     Active.line.points(linepoints);
 }
 
 Field.stage.on('contextmenu', (e) => {
     e.evt.preventDefault();
-});  
+});
 
 Field.stage.on('pointermove', function () {
-    switch (SideBar.current_tool)
-    {
+    switch (SideBar.current_tool) {
         case Tools.Draw:
             var pointerPos = Field.stage.getRelativePointerPosition();
             var x = pointerPos.x;
-            var y = pointerPos.y;  
-            Active.line.points(Active.line.points().slice(0, -2).concat(x, y));  
+            var y = pointerPos.y;
+            Active.line.points(Active.line.points().slice(0, -2).concat(x, y));
             break;
     }
 
 });
-  
+
 Field.stage.on('click', (e) => {
     if (e.evt.button === 2) {
-        switch (SideBar.current_tool)
-        {
+        switch (SideBar.current_tool) {
             case Tools.Draw:
+                activateEditMode();
+                break;
+            case Tools.Insert:
                 activateEditMode();
                 break;
             case Tools.Edit:
                 selected_point?.deselect();
                 break;
-
         }
     }
     else {
-        switch (SideBar.current_tool)
-        {
+        switch (SideBar.current_tool) {
             case Tools.Draw:
                 var pointerPos = Field.stage.getRelativePointerPosition();
                 var x = pointerPos.x;
-                var y = pointerPos.y; 
+                var y = pointerPos.y;
                 Active.line.points(Active.line.points().concat([x, y]));
-            
+
                 var circle = new Konva.Circle({
                     x: x,
                     y: y,
@@ -104,7 +163,7 @@ Field.stage.on('click', (e) => {
                 circle.on("pointerdown", () => {
                     point.select();
                 })
-            
+
                 Active.pointslayer.add(circle);
                 break;
         }
@@ -112,11 +171,11 @@ Field.stage.on('click', (e) => {
 });
 
 document.addEventListener('keyup', (e) => {
-    if(document.body == document.activeElement) {
+    if (document.body == document.activeElement) {
         const key = e.key;
         if (key === "Delete" || key === "Backspace") {
             console.log(document.activeElement)
-            if(SideBar.current_tool == Tools.Edit && selected_point !== null) {
+            if (SideBar.current_tool == Tools.Edit && selected_point !== null) {
                 const index = points.indexOf(selected_point);
                 if (index > -1) { // only splice array when item is found
                     points.splice(index, 1);
--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -2,3 +2,7 @@ export function map_range(value: number, input_start: number, input_end: number,
     var slope = (output_end - output_start) / (input_end - input_start);
     return output_start + slope * (value - input_start);
 }
+
+export function midpoint(x1, y1, x2, y2) {
+	return [(x1 + x2) / 2, (y1 + y2) / 2];
+}
